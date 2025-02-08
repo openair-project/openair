b439bb9fe785f75b7ba153285434094739d758d3
jack-davison <davison.jack.jd@gmail.com>
2025-02-08 13:45:13 +0000

    feat/docs/refactor: selectRunning
    - use dplyr over rle
    
    - roxygen2md
    
    - validate inputs
    
    - adds mode arg

--- R/selectRunning.R
+++ R/selectRunning.R
@@ -1,86 +1,101 @@
 #' Function to extract run lengths greater than a threshold
 #'
-#' Utility function to extract user-defined run lengths (durations) above a
-#' threshold
-#'
 #' This is a utility function to extract runs of values above a certain
 #' threshold. For example, for a data frame of hourly NOx values we would like
-#' to extract all those hours where the concentration is at least 500ppb for
+#' to extract all those hours where the concentration is at least 500 for
 #' contiguous periods of 5 or more hours.
 #'
-#' This function is useful, for example, for selecting pollution episodes from
-#' a data frame i.e. where concentrations remain elevated for a certain period
-#' of time. It may also be of more general use when analysing air pollution
-#' data. For example, \code{selectRunning} could be used to extract continuous
-#' periods of rainfall --- which could be important for particle
-#' concentrations.
+#' This function is useful, for example, for selecting pollution episodes from a
+#' data frame where concentrations remain elevated for a certain period of time.
+#' It may also be of more general use when analysing air pollution and
+#' atmospheric composition data. For example, [selectRunning()] could be used to
+#' extract continuous periods of rainfall --- which could be important for
+#' particle concentrations.
 #'
-#' @param mydata A data frame with a \code{date} field and at least one numeric
-#'   \code{pollutant} field to analyse.
-#' @param pollutant Name of variable to process. Mandatory.
-#' @param criterion Condition to select run lengths e.g. \code{">"} with select
-#'   data more than \code{threshold}.
-#' @param run.len Run length for extracting contiguous values of
-#'   \code{pollutant} above the \code{threshold} value.
-#' @param threshold The threshold value for \code{pollutant} above which data
-#'   should be extracted.
-#' @param result A new column \code{criterion} is returned with string to
-#'   identity whether condition was met.
+#' @param mydata A data frame with a `date` field and at least one numeric
+#'   `pollutant` field to analyse.
+#' @param pollutant Name of variable to process.
+#' @param criterion Condition to select run lengths e.g. `">"` with select data
+#'   more than `threshold`.
+#' @param run.len Run length for extracting contiguous values of `pollutant`
+#'   meeting the `criterion` in relation to the `threshold`.
+#' @param threshold The threshold value for `pollutant` above which data should
+#'   be extracted.
+#' @param result A vector of length 2, defining how to label the run lengths.
+#'   The first object should be the label for the `TRUE` label, and the second
+#'   the `FALSE` label - e.g., `c("yes", "no")`.
+#' @param name The name of the column to be appended to the data frame.
 #' @export
-#' @return Returns a data frame that meets the chosen criteria. See examples
-#'   below.
+#' @return A data frame
 #' @author David Carslaw
 #' @examples
-#'
-#' ## extract those hours where there are at least 5 consecutive NOx
-#' ## concentrations above 500ppb
-#'
+#' # extract those hours where there are at least 5 consecutive NOx
+#' # concentrations above 500 units
 #' mydata <- selectRunning(mydata, run.len = 5, threshold = 500)
 #'
-#' ## make a polar plot of those conditions...shows that those
-#' ## conditions are dominated by low wind speeds, not
-#' ## in-canyon recirculation
-#' \dontrun{polarPlot(mydata, pollutant = "nox", type = "criterion")}
-#'
-selectRunning <- function(mydata, pollutant = "nox",
+#' # make a polar plot of those conditions, which shows that those
+#' # conditions are dominated by low wind speeds, not
+#' # in-canyon recirculation
+#' \dontrun{
+#' polarPlot(mydata, pollutant = "nox", type = "criterion")
+#' }
+selectRunning <- function(mydata,
+                          pollutant = "nox",
                           criterion = ">",
-                          run.len = 5, threshold = 500,
-                          result = c("yes", "no")) {
-
-  ## function to return indices of running values above a certain threshold
-  ## make sure the time series is continuous in time with NO gaps
-  vars <- c("date", pollutant)
-
-  ## pad out missing data
+                          run.len = 5L,
+                          threshold = 500,
+                          result = c("yes", "no"),
+                          name = "criterion",
+                          mode = c("flag", "filter")) {
+  # check inputs are valid
+  mode <- rlang::arg_match(mode)
+  criterion <- rlang::arg_match(criterion, c("<", ">", "<=", ">=", "==", "!="), multiple = FALSE)
+  
+  # pad out missing data
   thedata <- date.pad(mydata)
-  mydata <- thedata ## save for later
-  thedata <- checkPrep(mydata, vars, type = "default", remove.calm = FALSE)
-
-  x <- thedata[[pollutant]]
-  my_formula <- paste("x", criterion, threshold)
-  rle.seq <- rle(eval(str2lang(my_formula)))
-  cumsum.seq <- cumsum(rle.seq$lengths)
-  myruns <- which(rle.seq$values == 1 & rle.seq$lengths >= run.len)
-
-  ends <- cumsum.seq[myruns]
-  newindex <- ifelse(myruns > 1, myruns - 1, 0)
-  starts <- cumsum.seq[newindex] + 1
-  if (0 %in% newindex) starts <- c(1, starts)
-  res <- data.frame(starts = starts, ends = ends)
-
-  if (nrow(res) > 0) {
-    ids <- lapply(1:nrow(res), function(x) seq(res[x, 1], res[x, 2]))
-    ids <- do.call(c, ids)
-
-    mydata$criterion <- result[2]
-    mydata$criterion[ids] <- result[1]
-    return(mydata)
-  } else {
-
-    mydata$criterion <- result[2]
-    return(mydata)
-
+  
+  # save input for later
+  mydata <- thedata
+  
+  # check input data - ensures `date` are in correct order
+  thedata <- checkPrep(
+    mydata,
+    Names = c("date", pollutant),
+    type = "default",
+    remove.calm = FALSE
+  )
+  
+  # construct expression
+  expr <- paste(pollutant, criterion, threshold)
+  
+  # calculate run lengths
+  thedata <-
+    thedata %>%
+    dplyr::mutate(
+      `__flag__` = rlang::eval_tidy(rlang::parse_expr(expr)),
+      `__run__` = dplyr::consecutive_id(`__flag__`)
+    ) %>%
+    dplyr::mutate(`__len__` = dplyr::n(), .by = "__run__") %>%
+    dplyr::mutate(
+      `__flag__` = dplyr::if_else(
+        condition = `__flag__` &
+          `__len__` >= run.len,
+        true = TRUE,
+        false = FALSE,
+        missing = FALSE
+      )
+    )
+  
+  # format outputs
+  if (mode == "flag") {
+    mydata <- mydata[thedata$`__flag__`, ]
   }
-
-
+  
+  if (mode == "append") {
+    mydata[[name]] <- thedata$`__flag__`
+    mydata[[name]] <- ifelse(mydata[[name]], result[1], result[2])
+  }
+  
+  # return
+  return(mydata)
 }
