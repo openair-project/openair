720ee845999a765a0fc1dc6d96f88551919c9bd7
jack-davison <davison.jack.jd@gmail.com>
2025-03-14 22:20:26 +0000

    style: markdownify & style polarDiff

--- R/polarDiff.R
+++ R/polarDiff.R
@@ -10,24 +10,20 @@
 #' pollutant. For example, data from two sites that are close to one another, or
 #' two co-located instruments.
 #'
-#' The analysis works by calculating the polar plot surface for the
-#' \code{before} and \code{after} periods and then subtracting the \code{before}
-#' surface from the \code{after} surface.
+#' The analysis works by calculating the polar plot surface for the `before` and
+#' `after` periods and then subtracting the `before` surface from the `after`
+#' surface.
 #'
 #' @inheritParams polarPlot
-#' @param before A data frame that represents the "before" case. See
-#'   [polarPlot()] for details of different input requirements.
-#' @param after A data frame that represents the "after" case. See [polarPlot()]
-#'   for details of different input requirements.
-#' @inheritDotParams polarPlot -pollutant -x -limits -plot
+#' @param before,after Data frames representing the "before" and "after" cases.
+#'   See [polarPlot()] for details of different input requirements.
+#' @inheritDotParams polarPlot -mydata -pollutant -x -limits -plot -type
 #' @family polar directional analysis functions
 #' @return an [openair][openair-package] plot.
 #' @export
 #'
 #' @examples
-#'
 #' \dontrun{
-#'
 #' before_data <- selectByDate(mydata, year = 2002)
 #' after_data <- selectByDate(mydata, year = 2003)
 #'
@@ -35,7 +31,6 @@
 #'
 #' # with some options
 #' polarDiff(before_data, after_data, pollutant = "no2", cols = "RdYlBu", limits = c(-20, 20))
-#'
 #' }
 polarDiff <- function(
     before,
@@ -44,33 +39,31 @@ polarDiff <- function(
     x = "ws",
     limits = NULL,
     plot = TRUE,
-    ...
-) {
-  if (is.null(limits)){
+    ...) {
+  if (is.null(limits)) {
     limits <- NA
   }
 
   # extra args setup
   Args <- list(...)
-  
+
   # variables needed, check for York regression where x and y error needed
   if (all(c("x_error", "y_error") %in% names(Args))) {
-    
     vars <- c(x, "wd", pollutant, Args$x_error, Args$y_error)
-    
   } else {
-    
     vars <- c(x, "wd", pollutant)
-    
   }
-  
 
   # check variables exists
   before <- checkPrep(before, vars,
-                      "default", remove.calm = FALSE)
+    "default",
+    remove.calm = FALSE
+  )
 
   after <- checkPrep(after, vars,
-                     "default", remove.calm = FALSE)
+    "default",
+    remove.calm = FALSE
+  )
 
   # need to pass on use limits only to final plot
   Args$new_limits <- limits
@@ -82,31 +75,37 @@ polarDiff <- function(
   all_data <- bind_rows(before, after)
 
   polar_plt <- polarPlot(all_data,
-                         pollutant = pollutant,
-                         x = x,
-                         type = "period",
-                         plot = FALSE,
-                         ...)
-  
-  if (length(pollutant > 1))
+    pollutant = pollutant,
+    x = x,
+    type = "period",
+    plot = FALSE,
+    ...
+  )
+
+  if (length(pollutant > 1)) {
     pollutant <- paste(pollutant, collapse = "_")
+  }
 
   polar_data <- pivot_wider(polar_plt$data,
-                            id_cols = u:v,
-                            names_from = period,
-                            values_from = z) %>%
+    id_cols = u:v,
+    names_from = period,
+    values_from = z
+  ) %>%
     mutate({{ pollutant }} := after - before,
-           {{ x }} := (u ^ 2 + v ^ 2) ^ 0.5,
-           wd = 180 * atan2(u, v) / pi,
-           wd = ifelse(wd < 0, wd + 360, wd))
+      {{ x }} := (u^2 + v^2)^0.5,
+      wd = 180 * atan2(u, v) / pi,
+      wd = ifelse(wd < 0, wd + 360, wd)
+    )
 
   # other arguments
   # colours
   Args$cols <- if ("cols" %in% names(Args)) {
     Args$cols
   } else {
-    c("#002F70", "#3167BB", "#879FDB", "#C8D2F1", "#F6F6F6",
-      "#F4C8C8", "#DA8A8B", "#AE4647", "#5F1415")
+    c(
+      "#002F70", "#3167BB", "#879FDB", "#C8D2F1", "#F6F6F6",
+      "#F4C8C8", "#DA8A8B", "#AE4647", "#5F1415"
+    )
   }
 
   # limits
@@ -142,18 +141,19 @@ polarDiff <- function(
 
   # final plot
   plt <-
-    polarPlot(polar_data, pollutant = pollutant,
-              x = x, plot = plot,
-              cols = Args$cols,
-              limits = Args$limits,
-              force.positive = FALSE,
-              alpha = Args$alpha,
-              key = Args$key,
-              par.settings = Args$par.settings)
+    polarPlot(polar_data,
+      pollutant = pollutant,
+      x = x, plot = plot,
+      cols = Args$cols,
+      limits = Args$limits,
+      force.positive = FALSE,
+      alpha = Args$alpha,
+      key = Args$key,
+      par.settings = Args$par.settings
+    )
 
   # return
   output <- list(plot = plt$plot, data = polar_data, call = match.call())
 
   invisible(output)
 }
-
