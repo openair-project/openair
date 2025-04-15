41f4f2be0df8e78c581a86d2f9e366d94d75b0a4
jack-davison <davison.jack.jd@gmail.com>
2025-04-15 21:01:58 +0100

    fix: delete duplicate selectByDate

--- R/selectByDate.R
+++ R/selectByDate.R
@@ -4,23 +4,24 @@
 #' periods (month, year, etc.). All options are applied in turn, meaning this
 #' function can be used to select quite complex dates simply.
 #'
-#' @param mydata A data frame containing a \code{date} field in Date or POSIXct format.
-#' @param start A start date or date-time string in the form d/m/yyyy, m/d/yyyy, d/m/yyyy HH:MM,
-#' m/d/yyyy HH:MM, d/m/yyyy HH:MM:SS or m/d/yyyy HH:MM:SS.
-#' @param end See \code{start} for format.
-#' @param year A year or years to select e.g. \code{year = 1998:2004} to select
-#'   1998-2004 inclusive or \code{year = c(1998, 2004)} to select 1998 and 2004.
-#' @param month A month or months to select. Can either be numeric e.g.
-#'   \code{month = 1:6} to select months 1-6 (January to June), or by name e.g.
-#'   \code{month = c("January", "December")}. Names can be abbreviated to 3
-#'   letters and be in lower or upper case.
-#' @param day A day name or or days to select. \code{day} can be numeric (1 to
-#'   31) or character. For example \code{day = c("Monday", "Wednesday")} or
-#'   \code{day = 1:10} (to select the 1st to 10th of each month). Names can be
-#'   abbreviated to 3 letters and be in lower or upper case. Also accepts
-#'   \dQuote{weekday} (Monday - Friday) and \dQuote{weekend} for convenience.
-#' @param hour An hour or hours to select from 0-23 e.g. \code{hour = 0:12} to
-#'   select hours 0 to 12 inclusive.
+#' @param mydata A data frame containing a `date` field in Date or POSIXct
+#'   format.
+#' @param start A start date or date-time string in the form d/m/yyyy, m/d/yyyy,
+#'   d/m/yyyy HH:MM, m/d/yyyy HH:MM, d/m/yyyy HH:MM:SS or m/d/yyyy HH:MM:SS.
+#' @param end See `start` for format.
+#' @param year A year or years to select e.g. `year = 1998:2004` to select
+#'   1998-2004 inclusive or `year = c(1998, 2004)` to select 1998 and 2004.
+#' @param month A month or months to select. Can either be numeric e.g. `month =
+#'   1:6` to select months 1-6 (January to June), or by name e.g. `month =
+#'   c("January", "December")`. Names can be abbreviated to 3 letters and be in
+#'   lower or upper case.
+#' @param day A day name or or days to select. `day` can be numeric (1 to 31) or
+#'   character. For example `day = c("Monday", "Wednesday")` or `day = 1:10` (to
+#'   select the 1st to 10th of each month). Names can be abbreviated to 3
+#'   letters and be in lower or upper case. Also accepts `"weekday"` (Monday -
+#'   Friday) and `"weekend"` for convenience.
+#' @param hour An hour or hours to select from 0-23 e.g. `hour = 0:12` to select
+#'   hours 0 to 12 inclusive.
 #' @export
 #' @author David Carslaw
 #' @examples
@@ -46,15 +47,23 @@
 #'     c("dec", "jan", "feb")
 #' )
 #'
-selectByDate <- function(mydata, start = "1/1/2008",
-                         end = "31/12/2008", year = 2008,
-                         month = 1, day = "weekday", hour = 1) {
+selectByDate <- function(
+  mydata,
+  start = "1/1/2008",
+  end = "31/12/2008",
+  year = 2008,
+  month = 1,
+  day = "weekday",
+  hour = 1
+) {
   ## extract variables of interest
   vars <- names(mydata)
 
   ## check data - mostly date format
   mydata <- checkPrep(
-    mydata, vars, "default",
+    mydata,
+    vars,
+    "default",
     remove.calm = FALSE,
     strip.white = FALSE
   )
@@ -67,7 +76,18 @@ selectByDate <- function(mydata, start = "1/1/2008",
     if (lubridate::is.Date(mydata$date)) {
       start <- lubridate::as_date(start, format = date_formats)
     } else {
-      start <- lubridate::parse_date_time(start, orders = c("dmy_HM", "dmy_HMS", "ymd_HM", "ymd_HMS", "dmy", "mdy", "ymd"))
+      start <- lubridate::parse_date_time(
+        start,
+        orders = c(
+          "dmy_HM",
+          "dmy_HMS",
+          "ymd_HM",
+          "ymd_HMS",
+          "dmy",
+          "mdy",
+          "ymd"
+        )
+      )
     }
 
     mydata <- filter(mydata, date >= start)
@@ -77,17 +97,26 @@ selectByDate <- function(mydata, start = "1/1/2008",
     if (lubridate::is.Date(mydata$date)) {
       end <- lubridate::as_date(end, format = date_formats)
     } else {
-      end <- lubridate::parse_date_time(end, orders = c("dmy_HM", "dmy_HMS", "ymd_HM", "ymd_HMS", "dmy", "mdy", "ymd"))
+      end <- lubridate::parse_date_time(
+        end,
+        orders = c(
+          "dmy_HM",
+          "dmy_HMS",
+          "ymd_HM",
+          "ymd_HMS",
+          "dmy",
+          "mdy",
+          "ymd"
+        )
+      )
     }
 
     mydata <- filter(mydata, date <= end)
   }
 
-
   if (!missing(year)) {
     mydata <- mydata[which(year(mydata$date) %in% year), ]
   }
-
 
   if (!missing(month)) {
     if (is.numeric(month)) {
@@ -97,10 +126,18 @@ selectByDate <- function(mydata, start = "1/1/2008",
 
       mydata <- mydata[which(month(mydata$date) %in% month), ]
     } else {
-      mydata <- subset(mydata, substr(tolower(format(
-        date,
-        "%B"
-      )), 1, 3) %in% substr(tolower(month), 1, 3))
+      mydata <- subset(
+        mydata,
+        substr(
+          tolower(format(
+            date,
+            "%B"
+          )),
+          1,
+          3
+        ) %in%
+          substr(tolower(month), 1, 3)
+      )
     }
   }
   if (!missing(hour)) {
@@ -124,8 +161,11 @@ selectByDate <- function(mydata, start = "1/1/2008",
       if (day[1] == "weekend") {
         days <- weekday.names[6:7]
       }
-      mydata <- subset(mydata, substr(tolower(format(date, "%A")), 1, 3) %in%
-        substr(tolower(days), 1, 3))
+      mydata <- subset(
+        mydata,
+        substr(tolower(format(date, "%A")), 1, 3) %in%
+          substr(tolower(days), 1, 3)
+      )
     }
   }
   mydata
