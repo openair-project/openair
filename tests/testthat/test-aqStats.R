1cab009fc8de6fdde9f5b5563c79f5d1af6555ea
jack-davison <davison.jack.jd@gmail.com>
2025-06-30 11:40:35 +0100

    test: update tests with new error behaviour

--- tests/testthat/test-aqStats.R
+++ tests/testthat/test-aqStats.R
@@ -1,8 +1,8 @@
 test_that("aqstats works", {
   testdat <- head(mydata, n = 100)
-  
+
   stats <- aqStats(testdat, c("no2", "o3", "pm10", "so2"))
-  
+
   expect_equal(
     names(stats),
     c(
@@ -27,18 +27,20 @@ test_that("aqstats works", {
       "days"
     )
   )
-  
+
   # check stats only calculated for specific pollutants
   expect_equal(stats$hours, c(0, NA, NA, NA))
   expect_equal(stats$roll.8.O3.gt.120, c(NA, 0, NA, NA))
   expect_equal(stats$days, c(NA, NA, 0, NA))
-  
+
   # transpose
   stats_t <- aqStats(testdat, c("no2", "o3", "pm10", "so2"), transpose = TRUE)
-  
-  expect_equal(names(stats_t),
-               c("default", "year", "date", "name", "no2", "o3", "pm10", "so2"))
-  
-  # warning
-  expect_warning(aqStats(rbind(testdat, testdat)))
+
+  expect_equal(
+    names(stats_t),
+    c("default", "year", "date", "name", "no2", "o3", "pm10", "so2")
+  )
+
+  # error if duplicate dates
+  expect_error(aqStats(rbind(testdat, testdat)))
 })
