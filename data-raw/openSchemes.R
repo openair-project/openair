26800747ed1c1c8411710a935d6a7da21568b414
jack-davison <davison.jack.jd@gmail.com>
2025-11-11 12:14:55 +0000

    chore: openSchemes tibble

--- data-raw/openSchemes.R
+++ data-raw/openSchemes.R
@@ -2,7 +2,8 @@
 
 # Edit in "data-raw/openSchemes.csv"
 openSchemes <- readr::read_csv("data-raw/openSchemes.csv") |>
-  dplyr::arrange(category, origin, scheme)
+  dplyr::arrange(category, origin, scheme) |>
+  dplyr::tibble()
 
 # write object
 usethis::use_data(openSchemes, overwrite = TRUE)
