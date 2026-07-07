fa757b172eff45f8cff77a874171055035e7a0f4
jack-davison <davison.jack.jd@gmail.com>
2026-07-07 13:55:40 +0100

    colour tweaks

--- R/openColours.R
+++ R/openColours.R
@@ -271,7 +271,8 @@ openColours <- function(
   direction[direction == 0] <- 1
 
   # user-supplied colour vectors
-  if (length(scheme) > 1L || !scheme %in% .all_schemes) {
+  # NB: WSP palette not for wider use so remains undocumented
+  if (length(scheme) > 1L || !scheme %in% c(.all_schemes, "wsp", "wsp2")) {
     if (length(scheme) > 1L && any(scheme %in% .all_schemes)) {
       cli::cli_abort(
         c(
@@ -333,7 +334,7 @@ openColours <- function(
       lightness,
       saturation
     )
-  } else if (scheme %in% names(.qual_schemes)) {
+  } else if (scheme %in% c(names(.qual_schemes), "wsp", "wsp2")) {
     .qualPalette(n_out, scheme, direction, alpha, lightness, saturation)
   }
 
@@ -1360,6 +1361,30 @@ openSchemes <- function(palette_type = c("seq", "div", "qual"), n = NULL) {
       "#97BBF5",
       "#9C6B4E",
       "#9498A0"
+    ),
+    wsp = c(
+      "#1594FD",
+      "#F35B05",
+      "#FFD10A",
+      "#496C1A",
+      "#B16F4F",
+      "#162E76",
+      "#C33900",
+      "#FEA513",
+      "#224807",
+      "#4C2C00"
+    ),
+    wsp2 = c(
+      "#1594FD",
+      "#162E76",
+      "#F35B05",
+      "#C33900",
+      "#FFD10A",
+      "#FEA513",
+      "#496C1A",
+      "#224807",
+      "#B16F4F",
+      "#4C2C00"
     )
   )
 
